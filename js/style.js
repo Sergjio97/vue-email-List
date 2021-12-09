d2e6870a5895b187044637cd9168bcef70bb192b
Sergjio97 <sergiocizmja@gmail.com>
2021-12-09 17:21:22 +0100

    fine esercizio

--- js/style.js
+++ js/style.js
@@ -1,4 +1,5 @@
 const app = new Vue({
+
     el: "#root",
 
     data: {
@@ -8,4 +9,21 @@ const app = new Vue({
     methods: {
 
     },
+    
+    created() {
+        for (let i = 0; i < 10; i++) {
+
+            let newMail = {
+                mail: null,
+            }
+
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((response) => {
+                newMail.mail = response.data.response;
+            });
+
+            this.listaMail.push(newMail);
+        }
+    },
+
 });
